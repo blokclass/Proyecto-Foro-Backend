b8b3c7b5fcfa31b3cd92c1e1481e44a2f3a9acba
JCGdev <53711291+JCGdev@users.noreply.github.com>
2023-04-10 19:10:38 +0200

    Added Sample Test class, just for letting know other devs how to implement unitary tests using JUnit.

---
+++ src/test/java/by/daw/ExampleTest.java
@@ -0,0 +1,18 @@
+package by.daw;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExampleTest {
+
+    public ExampleTest() {
+
+    }
+
+    @Test
+    public void doSomething(){
+        System.out.println("Sample test!");
+        assertTrue(true);
+    }
+
+}
